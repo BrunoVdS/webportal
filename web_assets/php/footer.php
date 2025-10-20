08ba167068acfcc0a4ab92bfbbfc397ae46f0f38
BrunoVdS <bruno.vds@me.com>
2025-10-20 10:36:52 +0200

    Update footer.php

--- web_assets/php/footer.php
+++ web_assets/php/footer.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
   <div class="footer-content">
-    <img src="images/apatch-logo.svg" alt="APATCH Identification &amp; Equipment logo" class="footer-logo">
+    <img src="images/apatch-logo.png" alt="APATCH Identification &amp; Equipment logo" class="footer-logo">
     <p>Created by APATCH, <?php echo date('Y'); ?></p>
   </div>
 </footer>
